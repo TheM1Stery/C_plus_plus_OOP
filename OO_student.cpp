bc2d7ec5cdb1476ddd046c20500f167d343f4d2e
Seymur Bagirov <megadestroyer230@gmail.com>
2021-10-10 00:10:12 +0400

    finish the assignment

--- OO_student.cpp
+++ OO_student.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 #include <string.h>
 #define SIZE 100
-
+#define GARBAGE "AMOGUUUUUS"
 class Student
 {
 	char* name;
@@ -26,9 +26,11 @@ public:
 		char* country_s = new char[size];
 		char* group = new char[size];
 		int day = 0, month = 0, year = 0;
+
+
 	}
-	Student(const char* name,int day, int month, int year, const char* number, const char* city_S,
-	const char* country_S, const char* school, const char* city_s, const char* country_s, const char* group)
+	Student(const char* name, int day, int month, int year, const char* number, const char* city_S,
+		const char* country_S, const char* school, const char* city_s, const char* country_s, const char* group)
 	{
 		constexpr size_t size = 100;
 
@@ -36,7 +38,7 @@ public:
 		this->month = month;
 		this->year = year;
 
-		
+
 		this->name = new char[size];
 		this->number = new char[size];
 		this->city_S = new char[size];
@@ -54,7 +56,7 @@ public:
 		strcpy_s(this->city_s, size, city_s);
 		strcpy_s(this->country_s, size, country_s);
 		strcpy_s(this->group, size, group);
-		
+
 	}
 
 
@@ -209,14 +211,14 @@ public:
 		}
 		strcpy_s(this->city_s, SIZE, city_s);
 	}
-	
-	const char* get_school_city(const char* city_s)
+
+	const char* get_school_city()
 	{
 		if (city_s == nullptr)
 		{
 			return "0";
 		}
-		strcpy_s(this->city_s, SIZE, city_s);
+		return city_s;
 	}
 
 	void set_school_country(const char* country_s)
@@ -237,8 +239,37 @@ public:
 		return country_s;
 	}
 
-	
+	void set_student_group(const char* group)
+	{
+		if (group == nullptr)
+		{
+			return;
+		}
+		strcpy_s(this->group, SIZE, group);
+	}
 
+	const char* get_student_group()
+	{
+		if (group == nullptr)
+		{
+			return "0";
+		}
+		return group;
+	}
+
+	//friend std::ostream& operator << (std::ostream& stream, Student*& student);
+	void info()
+	{
+		std::cout << "Student's name: " << get_name() << '\n';
+		std::cout << "Student's day of birth: " << get_day() << '.' << get_month() << '.' << get_year() << '\n';
+		std::cout << "Student's number: " << get_number() << '\n';
+		std::cout << "Student's city: " << get_student_city() << '\n';
+		std::cout << "Student's country: " << get_student_country() << '\n';
+		std::cout << "Student's school: " << get_school() << '\n';
+		std::cout << "Student's group: " << get_student_group() << '\n';
+		std::cout << "Student's school's city: " << get_student_city() << '\n';
+		std::cout << "Student's school's country: " << get_school_country() << '\n';
+	}
 	~Student()
 	{
 		delete[] name;
@@ -253,10 +284,92 @@ public:
 };
 
 
+class Point
+{
+	float x, y, z;
+public:
+	Point()
+	{
+		x = 0, y = 0, z = 0;
+	}
+	// Point() : x(this->x), y(this->y), z(this->z)
+	// {
+	// }
+	Point(float x, float y, float z)
+	{
+		this->x = x;
+		this->y = y;
+		this->z = z;
+	}
+	//friend std::ostream& operator<< (std::ostream& stream, Point*& point);
+	void info()
+	{
+		std::cout << "x: " << x << '\n';
+		std::cout << "y: " << y << '\n';
+		std::cout << "z: " << z << '\n';
+	}
+	void set_x(float x)
+	{
+		this->x = x;
+	}
+	float get_x()
+	{
+		return x;
+	}
+	void set_y(float y)
+	{
+		this->y = y;
+	}
+	float get_y()
+	{
+		return y;
+	}
+	void set_z(float z)
+	{
+		this->z = z;
+	}
+	float get_z()
+	{
+		return z;
+	}
+};
+
+
+// std::ostream& operator<< (std::ostream& stream, Point*& point)
+// {
+// 	stream << "Current position: \n";
+// 	stream << "x: " << point->x << '\n';
+// 	stream << "y: " << point->y << '\n';
+// 	stream << "z: " << point->z << '\n';
+
+// }
+
+// std::ostream& operator<< (std::ostream& stream, Student*& student)
+// {
+// 	stream << "Student's name: " << student->name << '\n';
+// 	stream << "Student's day of birth: " << student->day << '.' << student->month << '.' << student->year << '\n';
+// 	stream << "Student's number: " << student->number << '\n';
+// 	stream << "Student's city: " << student->city_S << '\n';
+// 	stream << "Student's country: " << student->country_S << '\n';
+// 	stream << "Student's school: " << student->school << '\n';
+// 	stream << "Student's group: " << student->group << '\n';
+// 	stream << "Student's school's city: " << student->city_s << '\n';
+// 	stream << "Student's school's country " << student->country_s << '\n';
+// 	return stream;
+// }
 
 
 
 int main()
 {
-	
+	//Student* student = new Student("Name", 26, 5, 2003, "000000", "City", "Country", "School", "City", "Country", "Group");
+	////std::cout << student;
+	//student->info();
+	//student->set_number("727272");
+	//std::cout << '\n';
+	//student->info();
+	//delete student;
+	Point* point = new Point(10, 20, 30);
+	point->info();
+
 }
