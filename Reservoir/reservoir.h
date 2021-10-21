16ba3fd5f1b532c09baad0018379895ae2b5b462
TheM1Stery <87154668+TheM1Stery@users.noreply.github.com>
2021-10-21 21:54:16 +0400

    Update reservoir.h

--- Reservoir/reservoir.h
+++ Reservoir/reservoir.h
@@ -15,15 +15,22 @@ class Reservoir
 public:
     Reservoir();
 
-    Reservoir(double depth, double width, double length);
+    Reservoir(const char* name, const char* type, double depth, double width, double length);
 
     Reservoir(const Reservoir& reservoir);
 
+    Reservoir& operator=(const Reservoir& reservoir);
+
+    const char* get_name();
+
+    const char* get_type();
+
+
     double get_area();
 
-    double get_volume();
-    
-    int compare(Reservoir& compared_reservoir);
+    double get_volume();            
+
+    int compare_name(Reservoir& compared_reservoir);
 
     int compare_area(Reservoir& compared_reservoir);
 
