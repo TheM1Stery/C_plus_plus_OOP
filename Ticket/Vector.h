752c7132c1e5dce1cff6b36ae57c910d724953ff
Seymur Bagirov <megadestroyer230@gmail.com>
2021-10-31 18:02:46 +0400

    add Vector.h to Ticket

---
+++ Ticket/Vector.h
@@ -0,0 +1,260 @@
+#pragma once
+#include <iostream>
+#include <initializer_list>
+
+template<typename T> class Vector
+{
+    size_t actual_size; // the size that array actually has
+    size_t fixed_size; // the size which the user gets
+    T* vector;
+public:
+    Vector() // default constructor 
+    {
+        actual_size = 0;
+        fixed_size = 0;
+        //vector = new T[actual_size];
+        vector = static_cast<T*>(::operator new[](actual_size));
+    }
+    Vector(size_t actual_size)
+        : actual_size(actual_size), fixed_size(0)   // constructor which reserves memory
+    {
+        if (actual_size < 0)
+        {
+            this->actual_size = 0;
+            //vector = new T[this->actual_size];
+            vector = static_cast<T*>(::operator new[](this->actual_size * sizeof(T)));
+            return;
+        }
+        vector = static_cast<T*>(::operator new[](actual_size));
+    }
+    Vector(std::initializer_list<T> list)  // constructor with initliazer list
+        : actual_size(list.size()), fixed_size(list.size())
+    {
+        //vector = new T[list.size()];
+        vector = static_cast<T*>(::operator new[](list.size() * sizeof(T)));
+        int i = 0;
+        for (auto& item : list)
+        {
+            vector[i++] = item;
+        }
+    }
+    Vector(const Vector& arr) // Copy constructor
+    {
+        //vector = new T[arr.actual_size];
+        vector = static_cast<T*>(::operator new[](arr.actual_size * sizeof(T)));
+        for (int i = 0; i < arr.fixed_size; i++)
+        {
+            vector[i] = arr.vector[i];
+        }
+        actual_size = arr.actual_size;
+        fixed_size = arr.fixed_size;
+    }
+
+
+    Vector(Vector&& arr) // Move constructor
+    {
+        vector = static_cast<T*>(::operator new[](arr.actual_size * sizeof(T)));
+        for (int i = 0; i < arr.fixed_size; i++)
+        {
+            vector[i] = std::move(arr.vector[i]);
+        }
+        fixed_size = arr.fixed_size;
+        actual_size = arr.actual_size;
+
+        arr.actual_size = 0;
+        arr.fixed_size = 0;
+    }
+
+
+    Vector& operator=(const Vector& arr) // copy assignment
+    {
+        ::operator delete[](vector);
+        //vector = new T[arr.actual_size];
+        vector = static_cast<T*>(::operator new[](arr.actual_size * sizeof(T)));
+        for (int i = 0; i < arr.fixed_size; i++)
+        {
+            vector[i] = arr.vector[i];
+        }
+        actual_size = arr.actual_size;
+        fixed_size = arr.fixed_size;
+        return *this;
+    }
+
+    Vector& operator = (Vector&& arr) // move assignment
+    {
+        ::operator delete[](vector);
+        vector = static_cast<T*>(::operator new[](arr.actual_size * sizeof(T)));
+        for (int i = 0; i < arr.fixed_size; i++)
+        {
+            vector[i] = std::move(arr.vector[i]);
+        }
+        fixed_size = arr.fixed_size;
+        actual_size = arr.actual_size;
+
+        arr.actual_size = 0;
+        arr.fixed_size = 0;
+    }
+
+
+    size_t size()    // get the size of the Vector    (fixed_size)
+    {
+        return fixed_size;
+    }
+    size_t capacity() // get the capacity of the Vector  (actual_size)
+    {
+        return actual_size;
+    }
+    void reserve(size_t reserved_size) // Reserve memory for the Vector
+    {
+        if (empty())
+        {
+            ::operator delete[](vector);
+            //vector = new T[reserved_size];
+            vector = static_cast<T*>(::operator new[](reserved_size * sizeof(T)));
+            actual_size = reserved_size;
+            return;
+        }
+        if (reserved_size <= capacity())
+        {
+            return;
+        }
+        //T* new_arr = new T[reserved_size];
+        T* new_arr = static_cast<T*>(::operator new[](reserved_size * sizeof(T)));
+        for (int i = 0; i < fixed_size; i++)
+        {
+            new_arr[i] = std::move(vector[i]);
+        }
+        ::operator delete[](vector);
+        vector = new_arr;
+        actual_size = reserved_size;
+    }
+    T& operator[](int index) // access the element of the array
+    {
+        return vector[index];
+    }
+    void operator=(std::initializer_list<T> list) // reassign the values of the array using initiliazer list
+    {
+        if (list.size() > actual_size)
+        {
+            reserve(list.size());
+        }
+        int i = 0;
+        for (auto& item : list)
+        {
+            vector[i++] = item;
+        }
+        fixed_size = list.size();
+    }
+
+
+    T& at(int index) // access the element of the array
+    {
+        if (index < 0 || index >= fixed_size)
+        {
+            throw std::out_of_range("out of range");
+        }
+        return vector[index];
+    }
+    bool empty() // check if the array is empty
+    {
+        if (!size())
+        {
+            return true;
+        }
+        return false;
+    }
+    void push_back(const T& element) // put the element to the end of the array
+    {
+        if (capacity() == size())
+        {
+            if (capacity() == 0)
+            {
+                actual_size++;
+            }
+            reserve(actual_size * 2);
+        }
+        vector[fixed_size] = element;
+        fixed_size++;
+    }
+
+    void push_back(T&& element) // put the element to the end of the array
+    {
+        if (capacity() == size())
+        {
+            if (capacity() == 0)
+            {
+                actual_size++;
+            }
+            reserve(actual_size * 2);
+        }
+        vector[fixed_size] = std::move(element);
+        fixed_size++;
+    }
+
+    void assign(int count, T& value) // asssign values to the array
+    {
+        if (count > actual_size)
+        {
+            reserve(count);
+        }
+        for (int i = 0; i < count; i++)
+        {
+            vector[i] = value;
+        }
+        fixed_size = count;
+    }
+
+
+    void assign(int count, T&& value) // asssign values to the array
+    {
+        if (count > actual_size)
+        {
+            reserve(count);
+        }
+        for (int i = 0; i < count; i++)
+        {
+            vector[i] = value;
+        }
+        fixed_size = count;
+    }
+
+
+    void assign(std::initializer_list<T> list) // assign values to the array using initiliazer list
+    {
+        if (list.size() > actual_size)
+        {
+            reserve(list.size());
+        }
+        int i = 0;
+        for (auto& item : list)
+        {
+            vector[i++] = item;
+        }
+        fixed_size = list.size();
+    }
+    void pop_back() // remove the last element
+    {
+        if (!empty())
+        {
+            fixed_size--;
+        }
+    }
+    void erase(int index) // erase the element at the given index
+    {
+        if (empty())
+        {
+            return;
+        }
+        for (int i = index; i < fixed_size - 1; i++)
+        {
+            vector[i] = vector[i + 1];
+        }
+        fixed_size--;
+    }
+    ~Vector() // destructor
+    {
+        ::operator delete[](vector);
+    }
+
+
+};
