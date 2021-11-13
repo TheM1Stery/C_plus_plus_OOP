101f24b621e497bc992900886dacd99d35955b0d
Seymur Bagirov <megadestroyer230@gmail.com>
2021-11-13 21:21:47 +0400

    add rehash function to HashTable

--- HashTable/HashTable.h
+++ HashTable/HashTable.h
@@ -6,21 +6,48 @@
 template<typename K, typename T> class HashTable
 {
 	std::hash<K> hash_function;
-	size_t number_of_buckets;
 	size_t m_size;
-	size_t load_factor;
 	size_t max_load_factor = 1;
 	std::vector<std::vector<Pair<K,T>>> buckets;
-	size_t bucket_id(const K& key)
+	size_t bucket_id(const K& key) const
 	{
-		return hash_function(key) % number_of_buckets;
+		return hash_function(key) % buckets.size();
 	}
+	bool is_prime(size_t number)
+	{
+		if (number <= 1) return false;
+		if (number <= 3) return true;
+		if (number % 2 == 0 || number % 3 == 0)
+		{
+			return false;
+		}
+		for (long long i = 5; i <= sqrt(number); i += 6)
+		{
+			if (number % i == 0 || number % (i + 2) == 0)
+			{
+				return false;
+			}
+		}
+		return true;
+		
+	}
+
+	size_t next_prime(size_t number)
+	{
+		while (true)
+		{
+			if (is_prime(number))
+			{
+				return number;
+			}
+			number++;
+		}
+	}
+	
 public:
 	HashTable()
 	{
-		number_of_buckets = 8;
-		buckets.reserve(8);
-		buckets.assign(8, {});
+		buckets.resize(8);
 		m_size = 0;
 	}
 
@@ -34,11 +61,39 @@ public:
 	}*/
 
 
+	
+	void rehash(size_t count)
+	{
+		std::vector < Pair<K, T>> pairs;
+		pairs.reserve(m_size);
+		for (int i = 0; i < buckets.size(); i++) // copy pairs to rehash them later(down below)
+		{
+			for (int j = 0; j < buckets[i].size(); j++)
+			{
+				pairs.push_back(buckets[i][j]);
+			}
+		}
+		
+		buckets.resize(count);
+		
+		for (int i = 0; i < pairs.size(); i++) // rehash
+		{
+			size_t index = bucket_id(pairs[i].first);
+			buckets[index].emplace_back(pairs[i].first, pairs[i].second);
+		}
+		
+	}
 
 	void insert(const Pair<K,T>& pair)
 	{
+		if (m_size + 1 > max_load_factor * buckets.size())
+		{
+			size_t num = next_prime(buckets.size() * 2);
+			rehash(num);
+			//rehash(buckets.size() * 8);
+		}
 		size_t index = bucket_id(pair.first);
-		buckets[index].push_back({ pair.first, pair.second });
+		buckets[index].emplace_back(pair.first, pair.second);
 		m_size++;
 	}
 
@@ -53,6 +108,7 @@ public:
 			}
 		}
 		insert({key,0});
+		index = bucket_id(key);
 		for (int i = 0; i < buckets[index].size(); i++)
 		{
 			if (buckets[index][i].first == key)
@@ -74,13 +130,42 @@ public:
 		}
 		throw std::out_of_range("Element not found in the table");
 	}
+
+
+	const T& at(const K& key) const
+	{
+		size_t index = bucket_id(key);
+		for (int i = 0; i < buckets[index].size(); i++)
+		{
+			if (buckets[index][i].first == key)
+			{
+				return buckets[index][i].second;
+			}
+		}
+		throw std::out_of_range("Element not found in the table");
+	}
 	
+	size_t empty() const
+	{
+		return m_size == 0;
+	}
+	
+
+
 	size_t size() const 
 	{
 		return m_size;
 	}
+	
+	size_t bucket_count() const
+	{
+		return buckets.size();
+	}
 
-
+	size_t load_factor()
+	{
+		return m_size / buckets.size();
+	}
 
 	
 };
--- HashTable/main.cpp
+++ HashTable/main.cpp
@@ -1,15 +1,10 @@
 ﻿#include <iostream>
 #include <unordered_map>
-#include "HashTable.h"
 
 
 
 int main()
 {
-	HashTable<std::string, int> table;
-	table["Hello"] = 1337;
-	std::cout << table.at("Salam") << '\n';
-	
 	
 }
 
